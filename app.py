ffd2df2a74d6e61a4294efb55d51b8c34c9180ae
Riikii Chen <chexiyua@sheridancollege.ca>
2025-11-23 18:36:26 -0500

    register finish

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import os
 import mysql.connector
@@ -16,7 +16,8 @@ conn = mysql.connector.connect(
     host=app.config['MYSQL_HOST'],
     user=app.config['MYSQL_USER'],
     password=app.config['MYSQL_PASSWORD'],
-    database=app.config['MYSQL_DB']
+    database=app.config['MYSQL_DB'],
+    port=8889
 )
 
 cursor = conn.cursor()
@@ -25,10 +26,34 @@ cursor = conn.cursor()
 def hompage():
     return render_template("homepage.html")
 
-@app.route("/")
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/process-register", methods=['POST'])
+def process_register():
+    username = request.form.get('username')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    role = "member"
+
+    cursor.execute('''INSERT INTO `User` (username, email, password, role)
+        VALUES (%s, %s, %s, %s)
+            ''', (username, email, password, role))
+    conn.commit()
+
+    return render_template("login.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/about")
 def about():
     return render_template("about.html")
 
-@app.route("/")
+@app.route("/contact")
 def contact():
     return render_template("contact.html")
